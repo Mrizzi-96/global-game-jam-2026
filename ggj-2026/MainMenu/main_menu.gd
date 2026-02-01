f005b3a465b4e4f4acdf2b53831d7be266c41be2
Ignacio Fiandrino <1069789+TucumetaL@users.noreply.github.com>
2026-02-01 10:35:55 +0100

    feat(): Add Credits

--- ggj-2026/MainMenu/main_menu.gd
+++ ggj-2026/MainMenu/main_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 var game_scene = "uid://io3piuiaqwig"
+var credits_scene = "uid://dag6xng5plusc"
 
 func _on_play_button_up() -> void:
 	get_tree().change_scene_to_file(game_scene)
@@ -8,3 +9,7 @@ func _on_play_button_up() -> void:
 
 func _on_quit_button_up() -> void:
 	get_tree().quit()
+
+
+func _on_credits_button_up() -> void:
+	get_tree().change_scene_to_file(credits_scene)
