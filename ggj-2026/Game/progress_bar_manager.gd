0bb53cb80e0d8e344469dbe30b5b489766d72706
Marco Camillo Rizzi <marco.rizzi.dev@proton.me>
2026-02-02 21:37:15 +0100

    wip(): progress bar to the left

--- ggj-2026/Game/progress_bar_manager.gd
+++ ggj-2026/Game/progress_bar_manager.gd
@@ -1,10 +1,13 @@
 extends TextureProgressBar
 @export var count_down_timer: Timer
 
+var speed = 67 # SIX SEVEN
+
 func _ready() -> void:
 	max_value = count_down_timer.wait_time
 	value = max_value
 
 func _process(delta: float) -> void:
 	# update progress values
-	value = count_down_timer.time_left
+	#value = count_down_timer.time_left
+	position += Vector2.LEFT * speed * delta
